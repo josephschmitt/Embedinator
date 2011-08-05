1b55d485435cc4cbd44f5300abc5462a27397813
Joseph Schmitt <joe@vimeo.com>
2011-08-05 17:43:48 -0400

    Change auto run setting by clicking and holding toolbar button.

--- Embedinator.safariextension/content.js
+++ Embedinator.safariextension/content.js
@@ -1,42 +1,36 @@
-function onSettingsReceived(settings) {
-    if (settings.autorun) {
-        embedinate();
-    }
-}
-
 if (window.top === window) {
+	//Listen for messages from global page
     safari.self.addEventListener('message', function(e) {
-        if (e.name == 'receiveSettings') {
-            onSettingsReceived(e.message.settings);
-        }
-		else if (e.name == 'embedinate') {
+        if (e.name == 'embedinate') {
 			embedinate();
 		}
     }, false);
 
+
+	//INIT
     safari.self.tab.dispatchMessage("getSettings");
-}
 
-//On YouTube website
-if (/youtube.com/.test(window.location.href)) {
-	var videoContainer = document.getElementById('watch-player'),
-		clip_id = (window.location.href.split('watch?v=')[1]).split('&')[0],
-		alert = document.getElementById('flash10-promo-div');
-	
-	//Replace with iframe
-	Embedinator.Util.replaceEmbed(videoContainer, Embedinator.YOUTUBE, {
-		width: videoContainer.clientWidth,
-		height: videoContainer.clientHeight,
-		clip_id: clip_id,
-		fullscreen: true
+	//On YouTube website
+	if (/youtube.com/.test(window.location.href)) {
+		var videoContainer = document.getElementById('watch-player'),
+			clip_id = (window.location.href.split('watch?v=')[1]).split('&')[0],
+			alert = document.getElementById('flash10-promo-div');
+
+		//Replace with iframe
+		Embedinator.Util.replaceEmbed(videoContainer, Embedinator.YOUTUBE, {
+			width: videoContainer.clientWidth,
+			height: videoContainer.clientHeight,
+			clip_id: clip_id,
+			fullscreen: true
+		});
+
+		//Remove No Flash alert
+		alert.parentNode.removeChild(alert);
+	}
+
+	//Enables iframe fullscreen on all Vimeo embeds
+	Array.prototype.slice.call(document.querySelectorAll('iframe[src*="vimeo"]')).forEach(function(iframe) {
+		iframe.setAttribute('webkitallowfullscreen', 'webkitallowfullscreen');
+		iframe.setAttribute('allowfullscreen', 'allowfullscreen');
 	});
-	
-	//Remove No Flash alert
-	alert.parentNode.removeChild(alert);
 }
-
-//Enables iframe fullscreen on all Vimeo embeds
-Array.prototype.slice.call(document.querySelectorAll('iframe[src*="vimeo"]')).forEach(function(iframe) {
-	iframe.setAttribute('webkitallowfullscreen', 'webkitallowfullscreen');
-	iframe.setAttribute('allowfullscreen', 'allowfullscreen');
-});
